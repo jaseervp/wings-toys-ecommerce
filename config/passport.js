139170e14fb0e00b5816d62926ab3cd9fb513e5c
jaseervp <muhammedjaseer595@gmail.com>
2026-03-12 09:45:32 +0530

    fix: resolve Google OAuth redirect_uri_mismatch by adding full callback path to localhost URI

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://wingstoys.online/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
 
 
     },
